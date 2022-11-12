02be22f15dff346ab5d915af002a347a7c73a951
christoph <venussibiya@gmail.com>
2022-11-12 20:45:51 +0200

    CS: Hardcoding email login details

--- phishing-detector/src/main/java/com/phishing/app/mail/EmailConfig.java
+++ phishing-detector/src/main/java/com/phishing/app/mail/EmailConfig.java
@@ -3,7 +3,6 @@ package com.phishing.app.mail;
 import com.phishing.app.repository.lookups.AppConfigRepository;
 import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.SystemUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,23 +16,14 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 @Configuration
 public class EmailConfig {
 
-    @Autowired
-    private AppConfigRepository appConfigRepository;
-
     @Bean
     public JavaMailSender getMailSender() throws Exception {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername(appConfigRepository.findByCode("MAIL_USER").getValue());
-        if (SystemUtils.IS_OS_WINDOWS) {
-            log.info("************THE APPLICATION IS RUNNING ON WINDOWS OS************");
-            mailSender.setPassword(appConfigRepository.findByCode("WINDOWS_MAIL_PASS").getValue());
-        } else {
-            log.info("************THE APPLICATION IS RUNNING ON LINUX BASED OS************");
-            mailSender.setPassword(appConfigRepository.findByCode("LINUX_MAIL_PASS").getValue());
-        }
+        mailSender.setUsername("myprojects.cs@gmail.com");
+        mailSender.setPassword("jvgtkszjvexnpjhb");
         Properties javaMailProperties = new Properties();
         javaMailProperties.put("mail.smtp.starttls.enable", "true");
         javaMailProperties.put("mail.smtp.auth", "true");
